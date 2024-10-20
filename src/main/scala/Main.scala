57cb2f8294df98c8a9cbf75531781d32c9e03e73
Begüm Peker <pbegum.2003@gmail.com>
2024-10-20 18:18:04 +0200

    tested code and created a test game

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,78 +1,33 @@
 import scala.util.Random
 
 object Main {
-  case class GameBoard(cards: List[Card],
-    playerStacks: List[List[Card]],
-    centerStack: List[Card],
-    drawPile: List[Card],
-    discardPile: List[Card]
-    ) {
-    def shuffle(): GameBoard = {
-      GameBoard(Random.shuffle(cards), playerStacks, centerStack, drawPile, discardPile)
-    }
-    def draw(): (Card, GameBoard) = {
-      val drawnCard = drawPile.head //draw first card from pile
-      val updatedDrawPile = drawPile.tail //tail has all elements except the first in its list; returns list after the first element
-      val updatedGameBoard = copy(drawPile = updatedDrawPile) //actualize gameboard
-      (drawnCard, updatedGameBoard) // return tuple
-    }
-    def getDrawPile: List[Card] = drawPile
-  }
-
-  case class PlayerHand(cards: List[Card]) {}
-
-  case class GameState(
-      players: List[PlayerHand],
-      gameBoard: GameBoard,
-      currentPlayerIndex: Int
-  )
-
-  /** Function to print the game board */
-  /*def printGameBoard(gameState: GameState): Unit = {
-    println("Player Hand:")
-    for (card <- gameState.players.head.cards) {
-      // print color and number
-      println(card.color + "-" + card.number)
-    }
-
-    println("\nPlayer Stacks:")
-    for (stack <- gameState.gameBoard.playerStacks) {
-      // run through the cards in each playerstack
-      for (card <- stack) {
-        // print color and number
-        println(card.color + "-" + card.number)
-      }
-      // separate player stacks
-      println("---")
-    }
-
-    println("\nCenter Stack:")
-    for (card <- gameState.gameBoard.centerStack) {
-      println(card.color + "-" + card.number)
-    }
-    println("\nDraw Pile:")
-    for (card <- gameState.gameBoard.drawPile) {
-      println(card.color + "-" + card.number)
-    }
-    println("\nDiscard Pile:")
-    for (card <- gameState.gameBoard.discardPile) {
-      println(card.color + "-" + card.number)
-    }
-  }*/
-
-  // main method
   def main(args: Array[String]): Unit = {
-    val playerHand = PlayerHand(List(Card("red", 3), Card("green", 5)))
-    val playerStacks =
-      List(Card("blue", 8), Card("red", 10))
-    val centerStack = List(List(Card("green", 1), Card("red", 6)))
-    val drawPile = List(Card("blue", 5))
-    val discardPile = List(Card("red", 7))
+    val numberPlayers =  2
+    val cardsPerPlayer =  7
 
-    val gameBoard = GameBoard(playerStacks, centerStack, drawPile, discardPile)
-    val gameState =
-      GameState(List(playerHand), gameBoard, currentPlayerIndex = 0)
+    //intialize empty: Gamenboard, PlayerHands, GameState
+    val initialGameBoard = GameBoard(List.empty[Card], List.empty[Card]).shuffleDeck()
+    val playerHands = List.fill(numberPlayers)(PlayerHand(List.empty[Card]))
+    var gameState = GameState(playerHands, initialGameBoard, 0, initialGameBoard.drawPile)
 
-    //printGameBoard(gameState)
+    //start game by dealing cards to players
+    def dealInitialCards(gameState: GameState, cardsPerPlayer: Int): GameState = {
+      var updatedGameState = gameState
+      for(_ <- 1 to cardsPerPlayer) {
+        for (playerIndex <- updatedGameState.players.indices) {
+          val (drawnCard, updatedHand, updatedBoard) = updatedGameState.gameBoard.drawCard(updatedGameState.players(playerIndex))
+          updatedGameState = updatedGameState.copy(players = updatedGameState.players.updated(playerIndex, updatedHand), gameBoard = updatedBoard)
+        }
+      }
+      updatedGameState
+    }
+    gameState = dealInitialCards(gameState, cardsPerPlayer)
+
+    for(i <- gameState.players.indices) {
+      println(s"player ${i + 1}'s hand:")
+      gameState.players(i).displayHand()
+      print("\n")
+    }
+
   }
 }

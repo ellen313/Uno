d5491397be9034d5b543fc425bd73a0d477fe0de
Begüm Peker <pbegum.2003@gmail.com>
2024-11-03 00:21:09 +0100

    added new methods to improve game logic

--- src/main/scala/GameBoard.scala
+++ src/main/scala/GameBoard.scala
@@ -1,5 +1,6 @@
 import scala.util.Random
 
+
 case class GameBoard(drawPile: List[Card], discardPile: List[Card]) {
 
   def createDeckWithAllCards(): List[Card] = {
@@ -50,7 +51,7 @@ case class GameBoard(drawPile: List[Card], discardPile: List[Card]) {
   def playCard(card: Card, gameState: GameState): GameState = {
     val currentPlayer = gameState.players(gameState.currentPlayerIndex)
 
-    if (!currentPlayer.cards.contains(card)) {
+    if (!currentPlayer.cards.contains(card)) { //check für später bei benutzereingabe
       throw new IllegalArgumentException("Card not in hand")
     }
 
@@ -63,27 +64,51 @@ case class GameBoard(drawPile: List[Card], discardPile: List[Card]) {
     val updatedDiscardPile = gameState.gameBoard.discardPile :+ card
     val updatedGameBoard = gameState.gameBoard.copy(discardPile = updatedDiscardPile)
 
-    gameState.copy(
+    val baseGameState = gameState.copy(
       players = gameState.players.updated(gameState.currentPlayerIndex, updatedHand),
       gameBoard = updatedGameBoard
     )
+    val finalGameState = card match {
+      case ActionCard(_, "reverse") => baseGameState.nextPlayer(baseGameState.copy(isReversed = !gameState.isReversed))
+      case ActionCard(_, "skip") => baseGameState.nextPlayer(baseGameState.nextPlayer(baseGameState))
+
+      case ActionCard(_, "draw two") =>
+        val nextIndex = baseGameState.nextPlayer(baseGameState).currentPlayerIndex
+        val updatedNextPlayerHand = (1 to 2).foldLeft(baseGameState.players(nextIndex)) { (hand, _) =>
+          baseGameState.gameBoard.drawCard(hand)._2
+        }
+        baseGameState.copy(players = baseGameState.players.updated(nextIndex, updatedNextPlayerHand))
+      case WildCard("wild draw four") =>
+        val nextIndex = baseGameState.nextPlayer(baseGameState).currentPlayerIndex
+        val updatedNextPlayerHand = (1 to 4).foldLeft(baseGameState.players(nextIndex)) { (hand, _) =>
+          baseGameState.gameBoard.drawCard(hand)._2
+        }
+        baseGameState.copy(players = baseGameState.players.updated(nextIndex, updatedNextPlayerHand))
+      case _ => baseGameState.nextPlayer(baseGameState)
+    }
+    finalGameState
   }
 
   def isValidPlay(card: Card, topCard: Option[Card]): Boolean = {
     topCard match {
-      case None => true 
+      case None => true
       case Some(tCard) =>
         (card, tCard) match {
+
+          case (WildCard("wild draw four"), WildCard("wild draw four")) => false
           case (WildCard(_), _) => true
           case (_, WildCard(_)) => true
+          case (ActionCard(color, "draw two"), ActionCard(topColor, "draw two")) => color == topColor
+
           case (NumberCard(color, number), NumberCard(topColor, topNumber)) =>
             color == topColor || number == topNumber
-          case (NumberCard(color, _), ActionCard(topColor, _)) =>
-            color == topColor
-          case (ActionCard(color, _), NumberCard(topColor, _)) =>
-            color == topColor
-          case (ActionCard(color, _), ActionCard(topColor, _)) =>
-            color == topColor
+
+          case (NumberCard(color, _), ActionCard(topColor, _)) => color == topColor
+          case (ActionCard(color, _), NumberCard(topColor, _)) => color == topColor
+
+          case (ActionCard(color, action), ActionCard(topColor, topAction)) =>
+            color == topColor || action == topAction
+
           case _ => false
         }
     }

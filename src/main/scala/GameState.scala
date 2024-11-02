ed93e720a478245288de6978b84ed6b00a15061f
Begüm Peker <pbegum.2003@gmail.com>
2024-11-03 00:21:09 +0100

    added new methods to improve game logic

--- src/main/scala/GameState.scala
+++ src/main/scala/GameState.scala
@@ -1,6 +1,13 @@
-case class GameState(
-                      players: List[PlayerHand],
-                      gameBoard: GameBoard,      
-                      currentPlayerIndex: Int,   
-                      allCards: List[Card]      
-                    )
+case class GameState( players: List[PlayerHand], gameBoard: GameBoard, currentPlayerIndex: Int,
+                      allCards: List[Card], isReversed: Boolean = false){
+
+  def nextPlayer(gameState: GameState): GameState= {
+    val playerCount = gameState.players.length
+    val nextIndex = if (gameState.isReversed) {
+      (gameState.currentPlayerIndex - 1 + playerCount) % playerCount
+    } else {
+      (gameState.currentPlayerIndex + 1) % playerCount
+    }
+    gameState.copy(currentPlayerIndex = nextIndex)
+  }
+}

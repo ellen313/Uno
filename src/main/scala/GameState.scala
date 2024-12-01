6743fae3cd10142bb2fe558e5e55065a372805f2
bgmpkr <pbegum.2003@gmail.com>
2024-12-01 14:37:55 +0100

    added new methods

--- src/main/scala/GameState.scala
+++ src/main/scala/GameState.scala
@@ -10,4 +10,38 @@ case class GameState( players: List[PlayerHand], gameBoard: GameBoard, currentPl
     }
     gameState.copy(currentPlayerIndex = nextIndex)
   }
+
+  def dealInitialCards(cardsPerPlayer: Int): GameState = {
+    var updatedGameState = this
+    for (_ <- 1 to cardsPerPlayer) {
+      for (playerIndex <- updatedGameState.players.indices) {
+        val (drawnCard, updatedHand, updatedBoard) = updatedGameState.gameBoard.drawCard(updatedGameState.players(playerIndex))
+        updatedGameState = updatedGameState.copy(
+          players = updatedGameState.players.updated(playerIndex, updatedHand),
+          gameBoard = updatedBoard)
+      }
+    }
+    updatedGameState
+  }
+
+  def checkForWinner(): Option[Int] = {
+    players.zipWithIndex.find { case (hand, _) =>
+      hand.isEmpty || (hand.hasUno && hand.hasSaidUno)
+    } match {
+      case Some((_, winnerIndex)) =>
+        println(s"Player ${winnerIndex + 1} wins!")
+        Some(winnerIndex)
+      // no winner found:
+      case None =>
+        None
+    }
+  }
+
+  def playerSaysUno(playerIndex: Int): GameState = {
+    val updatedPlayers = players.updated(
+      playerIndex,
+      players(playerIndex).sayUno()
+    )
+    copy(players = updatedPlayers)
+  }
 }

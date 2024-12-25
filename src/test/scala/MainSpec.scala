4f30901499e6b496967c6ef9d98ceaccb10f6f2e
bgmpkr <pbegum.2003@gmail.com>
2024-12-26 00:53:59 +0100

    added test for main, done 100%

---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.wordspec.AnyWordSpec
+
+class MainTest extends AnyWordSpec {
+
+  "The Main object" should {
+    "initialize the game and deal cards correctly" in {
+      
+      val out = new java.io.ByteArrayOutputStream()
+      Console.withOut(out) {
+        
+        Main.main(Array())
+        
+        val output = out.toString()
+        
+        assert(output.contains("player 1's hand:"))
+        assert(output.contains("player 2's hand:"))
+      }
+    }
+  }
+}

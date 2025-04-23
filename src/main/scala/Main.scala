04f93a3ca80190096c3fbb5a62d53a4ccdab6394
bgmpkr <pbegum.2003@gmail.com>
2025-04-24 00:02:27 +0200

    fix main with input recursive loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,45 +1,59 @@
 import model.*
 import scala.io.StdIn.readLine
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Welcome to UNO!")
+@main def runUno(): Unit = {
+  println("Welcome to UNO!")
 
-    val numberPlayers = readValidInt("How many players? (2-10): ", min = 2, max = 10)
-    val cardsPerPlayer =  7
+  val numberPlayers = readValidInt("How many players? (2-10): ", min = 2, max = 10)
+  val cardsPerPlayer =  7
 
-    // Initialize empty GameBoard, PlayerHands, and GameState
-    val initialGameBoard = GameBoard(List.empty[Card], List.empty[Card]).shuffleDeck()
+  // Initialize empty GameBoard, PlayerHands, and GameState
+  val initialGameBoard = GameBoard(List.empty[Card], List.empty[Card]).shuffleDeck()
 
-    val playerHands = List.fill(numberPlayers)(PlayerHand(List.empty[Card]))
-    var gameState = GameState(playerHands, initialGameBoard, 0, initialGameBoard.drawPile)
+  val playerHands = List.fill(numberPlayers)(PlayerHand(List.empty[Card]))
+  var gameState = GameState(playerHands, initialGameBoard, 0, initialGameBoard.drawPile)
 
-    gameState = gameState.dealInitialCards(cardsPerPlayer)
-    println("Let's start the Game!")
-    Thread.sleep(2000)
-    val tui = new UnoTui_neu(gameState)
-    tui.display()
+  gameState = gameState.dealInitialCards(cardsPerPlayer)
+  println("Let's start the Game!")
+  Thread.sleep(2000)
+  val tui = new UnoTui_neu(gameState)
+  tui.display()
+
+  inputLoop(tui)
+}
+
+def inputLoop(tui: UnoTui_neu): Unit = {
+  val input = readLine().trim
+  input match {
+    case "exit" => {
+      println("Game exited.")
+    }
+    case _ => {
+      tui.handleCardSelection(input)
+      inputLoop(tui)
+    }
   }
+}
 
-  def readValidInt(prompt: String, min: Int, max: Int): Int = {
-    var valid = false
-    var number = 0
-    while (!valid) {
-      print(prompt)
-      val input = readLine()
-      try {
-        val parsed = input.toInt
-        if (parsed >= min && parsed <= max) {
-          number = parsed
-          valid = true
-        } else {
-          println(s"Please enter a number between $min and $max.")
-        }
-      } catch {
-        case _: NumberFormatException =>
-          println("Invalid input. Please enter a number.")
+def readValidInt(prompt: String, min: Int, max: Int): Int = {
+  var valid = false
+  var number = 0
+  while (!valid) {
+    print(prompt)
+    val input = readLine()
+    try {
+      val parsed = input.toInt
+      if (parsed >= min && parsed <= max) {
+        number = parsed
+        valid = true
+      } else {
+        println(s"Please enter a number between $min and $max.")
+      }
+    } catch {
+      case _: NumberFormatException => {
+        println("Invalid input. Please enter a number.")
       }
     }
-    number
   }
+  number
 }

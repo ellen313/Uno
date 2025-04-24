bbc5bfaa17b354ebd9f55d167de1ce87926e8a44
ellen313 <ellenpepi447@gmail.com>
2025-04-24 17:29:03 +0200

    modified main and mainSpec

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,24 +3,29 @@ import scala.io.StdIn.readLine
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val game = runUno()
+    inputLoop(new UnoTui_neu(game))
+  }
+
+  def runUno(numberPlayers: Option[Int] = None, cardsPerPlayer: Int = 7): GameState = {
     println("Welcome to UNO!")
 
-    val numberPlayers = readValidInt("How many players? (2-10): ", min = 2, max = 10)
-    val cardsPerPlayer = 7
+    val players = numberPlayers.getOrElse(readValidInt("How many players? (2-10): ", min = 2, max = 10))
 
     // Initialize empty GameBoard, PlayerHands, and GameState
     val initialGameBoard = GameBoard(List.empty[Card], List.empty[Card]).shuffleDeck()
 
-    val playerHands = List.fill(numberPlayers)(PlayerHand(List.empty[Card]))
+    val playerHands = List.fill(players)(PlayerHand(List.empty[Card]))
     var gameState = GameState(playerHands, initialGameBoard, 0, initialGameBoard.drawPile)
 
     gameState = gameState.dealInitialCards(cardsPerPlayer)
     println("Let's start the Game!")
     Thread.sleep(2000)
+
     val tui = new UnoTui_neu(gameState)
     tui.display()
 
-    inputLoop(tui)
+    gameState
   }
 
   def inputLoop(tui: UnoTui_neu): Unit = {

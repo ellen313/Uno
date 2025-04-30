53b5c523eca19717747ad70834ccf9ebc58fadf3
ellen313 <ellenpepi447@gmail.com>
2025-04-30 17:35:55 +0200

    ObserverSpec finished

--- src/test/scala/model/ObserverSpec.scala
+++ src/test/scala/model/ObserverSpec.scala
@@ -1,5 +1,46 @@
 package model
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
 
-class ObserverSpec {
+class ObserverSpec extends AnyWordSpec {
+  "An Observable" when {
+    "an observer is added and notified" should {
+      "trigger the observer's update method" in {
+        var wasUpdated = false
 
+        val observer = new Observer {
+          override def update(): Unit = {
+            wasUpdated = true
+          }
+        }
+
+        val observable = new Observable {}
+
+        observable.addObserver(observer)
+        observable.notifyObservers()
+
+        wasUpdated shouldBe true
+      }
+    }
+
+    "an observer is removed" should {
+      "not be notified" in {
+        var wasUpdated = false
+
+        val observer = new Observer {
+          override def update(): Unit = {
+            wasUpdated = true
+          }
+        }
+
+        val observable = new Observable {}
+
+        observable.addObserver(observer)
+        observable.removeObserver(observer)
+        observable.notifyObservers()
+
+        wasUpdated shouldBe false
+      }
+    }
+  }
 }

37c574bebfeacf7378f41e03fc031554b24ba244
bgmpkr <pbegum.2003@gmail.com>
2025-05-15 18:51:06 +0200

    trying to fix tests - UnoStatesSpec 100%

---
+++ src/test/scala/de/htwg/se/uno/model/state/UnoStatesSpec.scala
@@ -0,0 +1,130 @@
+import de.htwg.se.uno.model.GameState
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.uno.model.state.*
+
+class UnoStatesSpec extends AnyWordSpec with Matchers {
+
+    // Dummy-GamePhase, die einfach sich selbst zurückgibt bei jedem Aufruf
+    class DummyState extends GamePhase {
+        override def playCard(): GamePhase = this
+        override def drawCard(): GamePhase = this
+        override def nextPlayer(): GamePhase = this
+        override def dealInitialCards(): GamePhase = this
+        override def checkForWinner(): GamePhase = this
+        override def playerSaysUno(): GamePhase = this
+        override def isValidPlay: Boolean = true
+    }
+
+  "UnoStates" should {
+
+        "delegate playCard call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.playCard()
+            unoStates.state shouldBe dummy
+        }
+
+        "delegate drawCard call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.drawCard()
+            unoStates.state shouldBe dummy
+        }
+
+        "delegate nextPlayer call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.nextPlayer()
+            unoStates.state shouldBe dummy
+        }
+
+        "delegate dealInitialCards call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.dealInitialCards()
+            unoStates.state shouldBe dummy
+        }
+
+        "delegate checkForWinner call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.checkForWinner()
+            unoStates.state shouldBe dummy
+        }
+
+        "delegate playerSaysUno call to currentState and update state" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+            val dummy = new DummyState
+            unoStates.setState(dummy)
+
+            unoStates.playerSaysUno()
+            unoStates.state shouldBe dummy
+        }
+
+        "tryPlayCard calls playCard if isValidPlay is true" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+
+            // Override DummyState to track if playCard was called
+            var played = false
+            val dummy = new DummyState {
+                override def playCard(): GamePhase = {
+                        played = true
+                        this
+                }
+            }
+
+            unoStates.setState(dummy)
+            unoStates.tryPlayCard()
+            played shouldBe true
+        }
+
+        "tryPlayCard prints 'Invalid play.' if isValidPlay is false" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+
+            val dummy = new DummyState {
+                override def isValidPlay: Boolean = false
+            }
+            unoStates.setState(dummy)
+
+            // To capture println output, use a stream
+      import java.io.ByteArrayOutputStream
+      import java.io.PrintStream
+
+            val outStream = new ByteArrayOutputStream()
+            Console.withOut(new PrintStream(outStream)) {
+                unoStates.tryPlayCard()
+            }
+
+            outStream.toString should include ("Invalid play.")
+        }
+
+        "setSelectedColor updates selectedColor" in {
+            val gameState = GameState(List(), 0, List(), false, List(), List())
+            val unoStates = new UnoStates(gameState)
+
+            unoStates.selectedColor shouldBe None
+            unoStates.setSelectedColor("red")
+            unoStates.selectedColor shouldBe Some("red")
+        }
+
+    }
+}

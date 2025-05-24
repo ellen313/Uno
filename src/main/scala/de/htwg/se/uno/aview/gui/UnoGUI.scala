cc2bd8d91026089e163e5fad7ab09079901d50a2
ellen313 <ellenpepi447@gmail.com>
2025-05-24 20:23:39 +0200

    implemented first GUI logic

--- src/main/scala/de/htwg/se/uno/aview/gui/UnoGUI.scala
+++ src/main/scala/de/htwg/se/uno/aview/gui/UnoGUI.scala
@@ -1,11 +1,17 @@
 package de.htwg.se.uno.aview.gui
 
+import de.htwg.se.uno.aview.UnoGame
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.geometry.Insets
-import scalafx.scene.layout.HBox
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.{Button, ButtonType}
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.{HBox, VBox}
+import scalafx.scene.paint.Color.*
 import scalafx.scene.paint.LinearGradient
+import scalafx.scene.paint.*
+import scalafx.scene.text.Text
 
 // properties: holds value whose changes can be observed:
 //  -> can be bound together, when one changes, the other does too
@@ -16,21 +22,39 @@ import scalafx.scene.paint.LinearGradient
 
 object UnoGUI extends JFXApp3 {
 
+  def startGame(): Unit = {
+    val tui = UnoGame.runUno()
+    UnoGame.inputLoop(tui)
+  }
+
   override def start(): Unit = {
     stage = new PrimaryStage {
       title = "Uno"
       scene = new Scene {
-        fill = Color.rgb(38, 38, 38)
-        content = new HBox {
+        fill = Color.rgb(40, 40, 40)
+        content = new VBox {
+          spacing = 30
           padding = Insets(50, 80, 50, 80)
+          alignment = Pos.Center
           children = Seq(
             new Text {
               text = "Uno"
-              style = "-fx-font: normal bold 100pt sans-serif"
+              style = "-fx-font: italic bold 100pt sans-serif"
               fill = new LinearGradient(
                 endX = 0,
                 stops = Stops(Red, DarkRed)
               )
+              effect = new DropShadow {
+                color = IndianRed
+                radius = 15
+                spread = 0.25
+              }
+            },
+            new Button("Start Game") {
+              style = "fx-font: italic bold; -fx-font-size: 20pt; -fx-background-color: #b22222; -fx-text-fill: white;"
+              onAction = _ => {
+                new Thread(() => startGame()).start()
+              }
             }
           )
         }

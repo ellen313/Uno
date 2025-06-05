32fc10488e8d242e17c178f8bc5c6c4474dd5f54
bgmpkr <pbegum.2003@gmail.com>
2025-06-05 18:49:38 +0200

    UnoTUISpec coverage 88%

--- src/test/scala/de/htwg/se/uno/aview/UnoTUISpec.scala
+++ src/test/scala/de/htwg/se/uno/aview/UnoTUISpec.scala
@@ -68,5 +68,142 @@ class UnoTUISpec extends AnyWordSpec with Matchers {
     "trigger update without throwing" in {
       noException should be thrownBy tui.update()
     }
+
+
+    "handle 'undo' command without throwing" in {
+      noException should be thrownBy tui.handleInput("undo")
+    }
+
+    "handle 'redo' command without throwing" in {
+      noException should be thrownBy tui.handleInput("redo")
+    }
+
+    "handle out-of-range index input" in {
+      noException should be thrownBy tui.handleInput("99")
+    }
+
+    "handle 'draw' when drawn card is not playable" in {
+      val unplayableCard = NumberCard("green", 7)
+      val currentCard = NumberCard("red", 3)
+
+      val state = gameState.copy(
+        drawPile = List(unplayableCard),
+        discardPile = List(currentCard, currentCard, currentCard, currentCard, currentCard),
+        players = List(PlayerHand(Nil, hasSaidUno = false))
+      )
+
+      GameBoard.updateState(state)
+
+      noException should be thrownBy tui.handleInput("draw")
+    }
+
+
+    "handle playing a WildCard via index input" in {
+      val hand = PlayerHand(List(WildCard("wild")), hasSaidUno = false)
+      val state = gameState.copy(
+        players = List(hand),
+        discardPile = List(NumberCard("red", 3))
+      )
+      GameBoard.updateState(state)
+
+      noException should be thrownBy tui.handleInput("0")
+    }
+
+
+    "not fail when discard pile is empty" in {
+      val emptyDiscardState = gameState.copy(discardPile = Nil)
+      GameBoard.updateState(emptyDiscardState)
+      noException should be thrownBy tui.display()
+    }
+
+
+    "display UNO message for other players" in {
+      val stateWithUnoPlayer = gameState.copy(players = List(
+        PlayerHand(List(NumberCard("red", 1)), hasSaidUno = false),
+        PlayerHand(List(NumberCard("blue", 2)), hasSaidUno = true)
+      ), currentPlayerIndex = 0)
+
+      GameBoard.updateState(stateWithUnoPlayer)
+      noException should be thrownBy tui.display()
+    }
+
+
+    "display selected color if it exists" in {
+      tui.selectedColor = Some("blue")
+      noException should be thrownBy tui.display()
+    }
+
+    "display error message when game state is not initialized" in {
+      GameBoard.reset()
+      val tuiNew = new UnoTUI(GameBoard)
+      noException should be thrownBy tuiNew.display()
+    }
+
+    "handle input gracefully when game state is not initialized" in {
+      GameBoard.reset()
+      val tuiNew = new UnoTUI(GameBoard)
+      noException should be thrownBy tuiNew.handleInput("draw")
+    }
+
+    "print wild card color options when choosing" in {
+      val input = Iterator("1")
+      val simulatedInput = () => input.next()
+      noException should be thrownBy tui.chooseWildColor(simulatedInput)
+    }
+
+
+    "display the player's hand with indices" in {
+      val player = PlayerHand(List(NumberCard("red", 4), NumberCard("green", 2)))
+      noException should be thrownBy tui.display()
+    }
+
+
+    "print winning message when a player has won" in {
+      val winningPlayer = PlayerHand(Nil, hasSaidUno = true)
+      val winningState = gameState.copy(players = List(winningPlayer))
+      GameBoard.updateState(winningState)
+      noException should be thrownBy tui.checkForWinner()
+    }
+
+    "set and get the shouldExit flag correctly" in {
+      tui.setShouldExit(true)
+      tui.shouldExit shouldBe true
+    }
+
+    "handle exception when checking UNO with uninitialized game state" in {
+      GameBoard.reset() // This will cause gameState to return a Failure
+      val tuiNew = new UnoTUI(GameBoard)
+      noException should be thrownBy tui.checkForWinner()
+    }
+
+    "handle drawing a non-playable card by skipping turn" in {
+      val unplayableCard = NumberCard("green", 9)
+      val hand = PlayerHand(Nil)
+      val drawPile = List(unplayableCard)
+      val discardPile = List(NumberCard("red", 3)) // ensures the green 9 isn't playable
+
+      val gameState = GameState(
+        players = List(hand),
+        currentPlayerIndex = 0,
+        allCards = List(),
+        isReversed = false,
+        drawPile = drawPile,
+        discardPile = discardPile,
+        selectedColor = None
+      )
+
+      GameBoard.updateState(gameState)
+      val tui = new UnoTUI(GameBoard)
+
+      noException should be thrownBy tui.handleInput("draw")
+    }
+
+    "print error when game state is not initialized in checkUno" in {
+      GameBoard.reset()
+      val tui = new UnoTUI(GameBoard)
+      noException should be thrownBy tui.checkForWinner()
+    }
+
+
   }
 }

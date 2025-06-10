8592c4a870d35941e1a600500a7d8f4dd54d65b7
ellen313 <ellenpepi447@gmail.com>
2025-06-10 16:37:46 +0200

    created Module for Dependency Injection

---
+++ src/main/scala/de/htwg/se/uno/UnoModule.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.uno
+
+import com.google.inject.AbstractModule
+import com.google.inject.name.Names
+import net.codingwell.scalaguice.ScalaModule
+
+class UnoModule extends AbstractModule with ScalaModule {
+  
+}
